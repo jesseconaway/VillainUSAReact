fa3c223c7e1cb36ce43a4f23645236813e24f2fd
jesseconaway <jesse@jesseconaway.com>
2020-06-07 09:47:07 -0500

    updates

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const compression = require("compression");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/api/hello", (req, res) => {
-  res.send({ express: "Hello From Express" });
-});
+app.use(helmet());
+app.use(compression());
 
 const products = require("./products.json");
 
@@ -17,12 +18,10 @@ app.post("/", (req, res) => {
   res.header("Content-Type", "application/json").send(JSON.stringify(products));
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "/build")));
 
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
-}
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/build", "index.html"));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
